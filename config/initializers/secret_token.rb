3da43b3aa543f2d79134615652cff2af5669526e
Ken Lu <ken@seeice.com>
2012-12-18 08:02:43 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FriendThings::Application.config.secret_token = 'e023c583569ffa7bc8ad3d82674ba6673dc9f7cc5e1cac6fea1b7cc1ca2aeba23e33483e33c2c8e0285be1d7b120abc5dddf7ca18466b3d0428fc46b3b6a09a9'
